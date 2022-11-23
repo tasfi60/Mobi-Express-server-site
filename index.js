83074a9a85265336dab1b9c9684f8ca4e5bd9850
tasfi60 <tasfiagirl@gmail.com>
2022-11-24 01:00:19 +0600

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/',(req,res) => {
+    res.send('Simple node Server is Running');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
